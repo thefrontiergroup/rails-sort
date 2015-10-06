2b23015e8e39909feb3f9acda78fef3e1094e92a
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-10-06 10:44:56 +0800

    Files in correct location

--- lib/rails-sortify.rb
+++ lib/rails-sortify.rb
@@ -1,17 +1,5 @@
-# -*- encoding: utf-8 -*-
-$:.push File.expand_path("../lib", __FILE__)
+require 'rails_sortify_helper'
 
-Gem::Specification.new do |s|
-  s.name        = "rails-sortify"
-  s.version     = "0.1.0"
-  s.authors     = ["Jordan Maguire", "Vinicius Osiro"]
-  s.email       = ["jordan@thefrontiergroup.com.au", "vinny@thefrontiergroup.com.au"]
-  s.homepage    = "https://github.com/thefrontiergroup/rails-sortify"
-  s.summary     = "Sorting helpers for Rails"
-  s.description = <<-EOF
-    Provide some view helpers and a sorting object to simplify adding sortable headers in a rails application
-  EOF
-
-  s.files         = Dir['README.md', 'lib/**/{*,.[a-z]*}']
-  s.require_paths = ["lib"]
+if defined?(Rails)
+  ActionView::Base.include RailsSortifyHelper
 end
---
+++ rails-sortify.gemspec
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |s|
+  s.name        = "rails-sortify"
+  s.version     = "0.1.0"
+  s.authors     = ["Jordan Maguire", "Vinicius Osiro"]
+  s.email       = ["jordan@thefrontiergroup.com.au", "vinny@thefrontiergroup.com.au"]
+  s.homepage    = "https://github.com/thefrontiergroup/rails-sortify"
+  s.summary     = "Sorting helpers for Rails"
+  s.description = <<-EOF
+    Provide some view helpers and a sorting object to simplify adding sortable headers in a rails application
+  EOF
+
+  s.files         = Dir['README.md', 'lib/**/{*,.[a-z]*}']
+  s.require_paths = ["lib"]
+end
